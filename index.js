46b55f75efe731bbd0a7f1e460207c8ae119f548
Cava <89451089+AchievementCava@users.noreply.github.com>
2021-09-26 19:24:00 +0800

    bot now comes online, no commands yet

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const Discord = require("discord.js");
+//calling dotenv module to get secret keys//
+require('dotenv').config()
+
+const { Client, Intents } = require("discord.js");
 const startTime = new Date();
 
-//client may also be called bot or host//
-const client = new Discord.Client();
+//client may also be called bot or host outside this project//
+//creating client//
+const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
 
-client.once('ready', () => {
-    console.log(concat("Hatta initiated contact at", startTime));
+client.on('ready', () => {
+    console.log("Hatta initiated contact at " + startTime);
 });
+
+
 //LAST LINE CODE//
-client.login("ODEwMTMyOTk2MjQ1OTQ2Mzc4.YCfNJQ.uVnUH04v_wRBOgMHcutwKd2yGvU");
+client.login(process.env.bot_token);
