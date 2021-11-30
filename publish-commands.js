d60ae79e55546f3a8c2dc0455c98c5dcb93e635e
Cava <89451089+AchievementCava@users.noreply.github.com>
2021-11-30 20:38:12 +0800

    added to gitignore

--- publish-commands.js
+++ publish-commands.js
@@ -15,12 +15,22 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    console.log(command);
     commands.push(command.data.toJSON());
-}
+};
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+(async () => {
+	try {
+		console.log('Started refreshing application slash commands.');
+
+		await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application slash commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
